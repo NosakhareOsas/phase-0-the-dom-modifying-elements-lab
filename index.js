d2951f5710288a91abbdfbadea9b5598b1f8152f
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-13 12:06:00 +0100

    Test Complete

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const main = document.getElementById("main");
 main.remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.textContent = "YOUR-NAME is the champion"
+newHeader.textContent = "YOUR-NAME is the champion";
 document.body.append(newHeader);
 
